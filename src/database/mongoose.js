0f7b5415d09b4e1c2283bdc9b4d21c03d9e2b499
Erhanero <erhancr7@gmail.com>
2022-02-11 13:02:28 +0200

    Add User model

--- src/database/mongoose.js
+++ src/database/mongoose.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
-const connectionString = "mongoose://localhost:27017/real-estate-db"
+const connectionString = "mongodb://localhost:27017/real-estate-db"
 
 async function initDatabase() {
     await mongoose.connect(connectionString);
 
 }
+
+module.exports = initDatabase
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const hbs = require("express-handlebars");
 const { PORT } = require("./constants");
 const routeController = require("./routes");
-
+const initDatabase = require("./database/mongoose");
 const app = express();
 
 
@@ -15,7 +15,14 @@ app.set("views", "./src/views");
 
 app.use(express.static("src"));
 
-
 app.use(routeController);
 
-app.listen(PORT, () => console.log(`Application is running on http://localhost:${PORT}`));
+initDatabase()
+    .then(() => {
+        app.listen(PORT, () => console.log(`Application is running on http://localhost:${PORT}`));
+
+    })
+    .catch(err => {
+        console.log(err, "Cannot connect database")
+    })
+
