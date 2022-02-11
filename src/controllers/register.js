06d88823dca9c8977e0f31a5e69cc08db7d2cf9f
Erhanero <erhancr7@gmail.com>
2022-02-11 14:18:24 +0200

    Add hashing password with bcrypt

--- src/controllers/register.js
+++ src/controllers/register.js
@@ -1,11 +1,15 @@
 const route = require("express").Router();
+const authService = require("../services/auth");
 
 route.get("/register", (req, res) => {
     res.render("register", { title: "Register Page" });
 });
 
-route.post("/register", (req, res) => {
+route.post("/register", async (req, res) => {
+    const { name, username, password, repeatPasswod } = req.body;
+    await authService.register({ name, username, password });
 
+    res.redirect("/login")
 })
 
 module.exports = route;
